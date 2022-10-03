52edb1b08187c9a7699e06c737f7c11f31b2a972
Arsenii Vladymyrov <arsenii.vladymyrov@bitmasters.co>
2022-10-03 14:46:23 +0300

    maps v5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
 <script>
 
 
-    let socket = new WebSocket("wss://secret-caverns-11171.herokuapp.com/");
+    let socket = new WebSocket("wss://secret-caverns-11171.herokuapp.com:8085/");
 
     socket.onopen = function(event) {
         console.log("Соединение установлено.");
